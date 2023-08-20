92f3c4db755b7500d1aa04046e849acf5313475c
Bhavani <bhavanisr021@gmail.com>
2023-08-20 13:15:41 +0530

    Second Commit

--- app/src/main/java/com/bhavani/demoapp/MainActivity.kt
+++ app/src/main/java/com/bhavani/demoapp/MainActivity.kt
@@ -4,19 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.bhavani.demoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.button.setOnClickListener {
+        val button= findViewById<Button>(R.id.button)
+        val textView=findViewById<TextView>(R.id.textView)
+
+        textView.setOnClickListener {
+            Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show()
+        }
+
+        button.setOnClickListener {
             Toast.makeText(this,"clicked",Toast.LENGTH_SHORT).show()
             val intent = Intent(this,MainActivity2::class.java)
             startActivity(intent)
--- app/src/main/java/com/bhavani/demoapp/MainActivity2.kt
+++ app/src/main/java/com/bhavani/demoapp/MainActivity2.kt
@@ -1,11 +1,22 @@
 package com.bhavani.demoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val helloTv=findViewById<TextView>(R.id.my)
+        helloTv.setOnClickListener {
+            Toast.makeText(this,"welcome back",Toast.LENGTH_SHORT).show()
+            val intent= Intent(this,MainActivity ::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
